0909e15f565c43c8e20daf10d0efd8c4cfa82827
mash811 <58508744+mash811@users.noreply.github.com>
2021-09-01 14:26:37 +0900

    Create gtag_new.js

---
+++ assets/gtag_new.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YPDQX2DGW9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YPDQX2DGW9');
+</script>
