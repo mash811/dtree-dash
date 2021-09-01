15831ecd45872bc5a8af6db4be27053a1b2503b3
mash811 <58508744+mash811@users.noreply.github.com>
2021-09-01 16:14:54 +0900

    add clientside_callback for node

--- app.py
+++ app.py
@@ -430,6 +430,17 @@ def displayTapNode(data):
         return "\n".join([label, imp, cond])
     else:
         return ""
+app.clientside_callback(
+    """
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-199659346-1', 'auto');
+    ga('send', 'pageview', '/click/node.html'); 
+    """
+)
 
 # In[19]:
 if __name__=='__main__':
